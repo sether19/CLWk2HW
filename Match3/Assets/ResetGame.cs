7711d0c1787513e520f340d4a511b865fe3d7976
seth s scott <sss782@nyu.edu>
2016-09-28 12:45:07 -0400

    Finalized audio

---
+++ Match3/Assets/ResetGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class ResetGame : MonoBehaviour {
+
+	public AudioSource serenity;
+	// Use this for initialization
+	void Start () {
+		//serenity = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ResetScene(){
+//		AudioSource audio = GetComponents <AudioSource> ()[0];
+//		audio.Play();
+	//	serenity.Play();
+		SceneManager.LoadScene(0);
+	}
+}
